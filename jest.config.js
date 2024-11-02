5f007ab8e679eda48b71d815724cf544bd847b08
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-11-02 08:05:59 -0400

    Changing roots in the jest.config.js file

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 ﻿module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src', '<rootDir>/__tests__'],
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
